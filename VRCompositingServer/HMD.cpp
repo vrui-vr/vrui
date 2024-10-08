d801d0b45ee61e05bd1aec13f432ecca6a4f283d
Oliver Kreylos <kreylos@cs.ucdavis.edu>
2024-10-08 10:30:21 -0700

    Starting new branch to fix VR compositor; fixed command line arguments in start-up script; case-insensitive display name matching

--- VRCompositingServer/HMD.cpp
+++ VRCompositingServer/HMD.cpp
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #include <stdlib.h>
 #include <Misc/StdError.h>
+#include <Misc/MessageLogger.h>
 #include <Vulkan/Instance.h>
 #include <Vulkan/PhysicalDeviceDescriptor.h>
 #include <Vulkan/ImageView.h>
@@ -241,7 +242,7 @@ void HMD::startVblankEstimator(void)
 
 uint64_t HMD::vsync(void)
 	{
-	uint64_t missed;
+	uint64_t missed=0;
 	
 	#if HMD_USE_VBLANK_EVENTS
 	
@@ -255,9 +256,14 @@ uint64_t HMD::vsync(void)
 	uint64_t newVblankCounter=swapchain.getVblankCounter();
 	
 	/* Update the vblank estimator through missed vblank events: */
-	missed=newVblankCounter-(vblankCounter+1);
-	for(uint64_t i=0;i<missed;++i)
-		vblankEstimator.update();
+	if(newVblankCounter!=vblankCounter)
+		{
+		missed=newVblankCounter-(vblankCounter+1);
+		for(uint64_t i=0;i<missed;++i)
+			vblankEstimator.update();
+		}
+	else
+		Misc::formattedConsoleWarning("HMD::vsync: Duplicate vsync counter %lu",vblankCounter);
 	
 	/* Update the vblank estimator: */
 	vblankEstimator.update(vblankSample);
