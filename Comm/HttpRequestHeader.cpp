03b1e3a3b18583eab21e0e8e402d06c07430f5c1
Oliver Kreylos <kreylos@cs.ucdavis.edu>
2026-04-01 17:33:28 -0700

    Added new non-blocking Comm::HttpRequestHeader class; move HTTP code in VRServerLauncher to non-blocking.

---
+++ Comm/HttpRequestHeader.cpp
@@ -0,0 +1,278 @@
+/***********************************************************************
+HttpRequestHeader - Helper class to parse general HTTP 1.1 request
+headers in a non-blocking fashion.
+Copyright (c) 2025-2026 Oliver Kreylos
+
+This file is part of the Portable Communications Library (Comm).
+
+The Portable Communications Library is free software; you can
+redistribute it and/or modify it under the terms of the GNU General
+Public License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+The Portable Communications Library is distributed in the hope that it
+will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with the Portable Communications Library; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA
+***********************************************************************/
+
+#include <Comm/HttpRequestHeader.h>
+
+#include <ctype.h>
+#include <stdlib.h>
+#include <Misc/StdError.h>
+#include <IO/File.h>
+
+namespace Comm {
+
+/**********************************
+Methods of class HttpRequestHeader:
+**********************************/
+
+HttpRequestHeader::HttpRequestHeader(void)
+	:parserState(RequestTypeString),
+	 requestType(Invalid),
+	 keepAlive(true), // keep-alive is default behavior in HTTP/1.1
+	 contentLength(~size_t(0))
+	{
+	}
+
+bool HttpRequestHeader::read(IO::File& file)
+	{
+	/* Bail out if the file's read buffer is empty: */
+	if(!file.canReadImmediately())
+		return parserState==Finished;
+	
+	/* Access the file's read buffer: */
+	void* buffer;
+	size_t bufferSize=file.readInBuffer(buffer);
+	char* rPtr=static_cast<char*>(buffer);
+	char* rEnd=rPtr+bufferSize;
+	
+	/* Run the parser state machine: */
+	do
+		{
+		switch(parserState)
+			{
+			case RequestTypeString:
+				/* Keep reading the request type string: */
+				while(rPtr!=rEnd&&*rPtr!=' ')
+					string.push_back(*(rPtr++));
+				
+				/* Check if the request type string is over: */
+				if(rPtr!=rEnd)
+					{
+					/* Determine the request type: */
+					if(string=="GET")
+						requestType=Get;
+					else if(string=="PUT")
+						requestType=Put;
+					else if(string=="POST")
+						requestType=Post;
+					else if(string=="OPTIONS")
+						requestType=Options;
+					else
+						{
+						requestType=Invalid;
+						throw Misc::makeStdErr(__PRETTY_FUNCTION__,"Invalid HTTP request type %s",string.c_str());
+						}
+					
+					/* Skip the separating space and start parsing the request URI: */
+					++rPtr;
+					string.clear();
+					parserState=RequestURI;
+					}
+				
+				break;
+			
+			case RequestURI:
+				/* Keep reading the request URI string: */
+				while(rPtr!=rEnd&&*rPtr!=' ')
+					requestUri.push_back(*(rPtr++));
+				
+				/* Check if the request URI string is over: */
+				if(rPtr!=rEnd)
+					{
+					/* Skip the separating space and start parsing the HTTP protocol version: */
+					++rPtr;
+					parserState=HTTPVersion;
+					}
+				
+				break;
+			
+			case HTTPVersion:
+				/* Keep reading the HTTP protocol version string: */
+				while(rPtr!=rEnd&&*rPtr!='\r')
+					string.push_back(*(rPtr++));
+				
+				/* Check if the HTTP protocol version string is over: */
+				if(rPtr!=rEnd)
+					{
+					if(string!="HTTP/1.1")
+						throw Misc::makeStdErr(__PRETTY_FUNCTION__,"Invalid HTTP protocol version %s",string.c_str());
+					
+					/* Skip the terminating carriage return and parse a request header start line end: */
+					++rPtr;
+					string.clear();
+					parserState=StartLineEnd;
+					}
+				
+				break;
+			
+			case StartLineEnd:
+				/* Check for and skip a line feed, then read the first header line: */
+				if(*rPtr!='\n')
+					throw Misc::makeStdErr(__PRETTY_FUNCTION__,"Invalid line end in request header's start line");
+				++rPtr;
+				parserState=HeaderLine;
+				
+				break;
+			
+			case HeaderLine:
+				/* Check for an empty line: */
+				if(*rPtr=='\r')
+					{
+					/* Skip the carriage return and parse a request header end: */
+					++rPtr;
+					parserState=HeaderEnd;
+					}
+				else
+					{
+					/* Start parsing a request header field name: */
+					parserState=HeaderFieldName;
+					}
+				
+				break;
+			
+			case HeaderFieldName:
+				/* Keep reading the request header field name: */
+				while(rPtr!=rEnd&&*rPtr!=':')
+					nameValue.name.push_back(tolower(*(rPtr++))); // Convert header field names to lowercase
+				
+				/* Check if the request header field name is over: */
+				if(rPtr!=rEnd)
+					{
+					/* Skip the separating colon and start parsing header request field value leading whitespace: */
+					++rPtr;
+					parserState=HeaderFieldValueLeadingSpace;
+					}
+				
+				break;
+			
+			case HeaderFieldValueLeadingSpace:
+				/* Keep skipping whitespace: */
+				while(rPtr!=rEnd&&*rPtr!='\r'&&isspace(*rPtr))
+					++rPtr;
+				
+				/* Check if whitespace is over: */
+				if(rPtr!=rEnd)
+					{
+					/* Check if we're at the line end: */
+					if(*rPtr=='\r')
+						{
+						/* Skip the carriage return and parse a request header line end: */
+						++rPtr;
+						parserState=HeaderLineEnd;
+						}
+					else
+						{
+						/* Start parsing a request header field value: */
+						parserState=HeaderFieldValue;
+						}
+					}
+				break;
+			
+			case HeaderFieldValue:
+				/* Keep reading the request header field value: */
+				while(rPtr!=rEnd&&*rPtr!='\r')
+					nameValue.value.push_back(*(rPtr++));
+				
+				/* Check if the request header field value is over: */
+				if(rPtr!=rEnd)
+					{
+					/* Skip the terminating carriage return and parse a request header line end: */
+					++rPtr;
+					parserState=HeaderLineEnd;
+					}
+				
+				break;
+			
+			case HeaderLineEnd:
+				/* Check for and skip a line feed: */
+				if(*rPtr!='\n')
+					throw Misc::makeStdErr(__PRETTY_FUNCTION__,"Invalid line end in request header");
+				++rPtr;
+				
+				/* Remove trailing whitespace from the just-parsed request header field value: */
+				while(!nameValue.value.empty()&&isspace(nameValue.value.back()))
+					nameValue.value.pop_back();
+				
+				/* Check for well-known request header fields: */
+				if(nameValue.name=="connection")
+					{
+					if(nameValue.value=="keep-alive")
+						keepAlive=true;
+					else if(nameValue.value=="close")
+						keepAlive=false;
+					else
+						throw Misc::makeStdErr(__PRETTY_FUNCTION__,"Invalid value \"%s\" in Connection header field",nameValue.value.c_str());
+					}
+				else if(nameValue.name=="content-length")
+					{
+					/* Try converting the field value field to an integer: */
+					const char* vStart=nameValue.value.c_str();
+					char* vEnd=0;
+					contentLength=size_t(strtoul(vStart,&vEnd,10));
+					if(vEnd==vStart||*vEnd!='\0')
+						throw Misc::makeStdErr(__PRETTY_FUNCTION__,"Invalid value \"%s\" in Content-Length header field",nameValue.value.c_str());
+					}
+				
+				/* Save the just-parsed request header field: */
+				headerFields.push_back(nameValue);
+				nameValue.name.clear();
+				nameValue.value.clear();
+				
+				/* Parse the next header line: */
+				parserState=HeaderLine;
+				
+				break;
+			
+			case HeaderEnd:
+				/* Check for and skip a line feed, then mark the header as finished: */
+				if(*rPtr!='\n')
+					throw Misc::makeStdErr(__PRETTY_FUNCTION__,"Invalid line end in request header");
+				++rPtr;
+				parserState=Finished;
+				
+				/* Mark the remainder of the file's read buffer as unread: */
+				file.putBackInBuffer(rEnd-rPtr);
+				rPtr=rEnd;
+				
+				break;
+			}
+		}
+	while(rPtr!=rEnd);
+	
+	return parserState==Finished;
+	}
+
+const std::string& HttpRequestHeader::getHeaderFieldValue(const char* headerFieldName) const
+	{
+	/* Find the header field of the given name: */
+	for(NameValueList::const_iterator hfIt=headerFields.begin();hfIt!=headerFields.end();++hfIt)
+		if(hfIt->name==headerFieldName)
+			{
+			/* Return the header field's value: */
+			return hfIt->value;
+			}
+	
+	/* The header field wasn't found; throw an exception: */
+	throw Misc::makeStdErr(__PRETTY_FUNCTION__,"Header field %s not found",headerFieldName);
+	}
+
+}
---
+++ Comm/HttpRequestHeader.h
@@ -0,0 +1,118 @@
+/***********************************************************************
+HttpRequestHeader - Helper class to parse general HTTP 1.1 request
+headers in a non-blocking fashion.
+Copyright (c) 2025-2026 Oliver Kreylos
+
+This file is part of the Portable Communications Library (Comm).
+
+The Portable Communications Library is free software; you can
+redistribute it and/or modify it under the terms of the GNU General
+Public License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+The Portable Communications Library is distributed in the hope that it
+will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with the Portable Communications Library; if not, write to the Free
+Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+02111-1307 USA
+***********************************************************************/
+
+#ifndef COMM_HTTPREQUEST_INCLUDED
+#define COMM_HTTPREQUEST_INCLUDED
+
+#include <stddef.h>
+#include <string>
+#include <vector>
+
+/* Forward declarations: */
+namespace IO {
+class File;
+}
+
+namespace Comm {
+
+class HttpRequestHeader
+	{
+	/* Embedded classes: */
+	public:
+	enum RequestType // Enumerated type for supported HTTP request types
+		{
+		Get,Put,Post,Options,Invalid
+		};
+	
+	struct NameValue // Structure to hold a name=value pair
+		{
+		/* Elements: */
+		public:
+		std::string name;
+		std::string value;
+		};
+	
+	typedef std::vector<NameValue> NameValueList; // Type for lists of name=value or name: value pairs
+	
+	private:
+	enum ParserState // Enumerated type for request parsing states
+		{
+		RequestTypeString,
+		RequestURI,
+		HTTPVersion,
+		StartLineEnd,
+		HeaderLine,HeaderFieldName,HeaderFieldValueLeadingSpace,HeaderFieldValue,HeaderLineEnd,
+		HeaderEnd,
+		Finished
+		};
+	
+	/* Elements: */
+	ParserState parserState; // Current request parser state
+	std::string string; // Buffer for the currently-parsed string
+	NameValue nameValue; // Buffer for a currently-parsed name/value pair
+	RequestType requestType; // The type of this request
+	std::string requestUri; // The request's URI
+	NameValueList headerFields; // List of request header field names and their values; header field names are converted to lowercase during parsing
+	bool keepAlive; // Flag if the client wants to keep the underlying TCP connection alive beyond this request
+	size_t contentLength; // Numerical value of the Content-Length header field if one was present; ~size_t(0) otherwise
+	
+	/* Constructors and destructors: */
+	public:
+	HttpRequestHeader(void); // Creates an invalid HTTP request header
+	
+	/* Methods: */
+	bool read(IO::File& file); // Continues parsing the HTTP request header from the given file's read buffer; does not block; returns true if the request header has been completely read; throws exceptions on parsing errors
+	bool isComplete(void) const // Returns true if the request header has been completely read
+		{
+		return parserState==Finished;
+		}
+	RequestType getRequestType(void) const // Returns the request header's type
+		{
+		return requestType;
+		}
+	const std::string& getRequestUri(void) const // Returns the request URI
+		{
+		return requestUri;
+		}
+	const NameValueList& getHeaderFields(void) const // Returns the list of request header fields; all field names are lowercase
+		{
+		return headerFields;
+		}
+	const std::string& getHeaderFieldValue(const char* headerFieldName) const; // Returns the value of the header field of the given name, which is in lowercase; throws exception if header field doesn't exist
+	bool getKeepAlive(void) const // Returns true if the client wants to keep the underlying TCP connection alive beyond this request
+		{
+		return keepAlive;
+		}
+	bool hasContentLength(void) const // Returns true if the header included a Content-Length field
+		{
+		return contentLength!=~size_t(0);
+		}
+	size_t getContentLength(void) const // Returns the numerical value of the Content-Length header field if one was present, or ~size_t(0) otherwise
+		{
+		return contentLength;
+		}
+	};
+
+}
+
+#endif
